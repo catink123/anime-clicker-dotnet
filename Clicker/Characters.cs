3edc828c5f060ed7249e6d3fb17de91ac7b98958
Catink123 <qwerty.qaz120@gmail.com>
2017-11-05 21:02:29 +0300

    xd

---
+++ Clicker/Characters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Clicker
+{
+    class Characters
+    {
+        public string name;
+        public Color color;
+        public string image;
+        public string faceImage;
+        public int dps;
+
+        public class Berserk
+        {
+            
+        }
+    }
+}
